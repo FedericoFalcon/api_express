1b39655608d6bc624a0a0a4c7d391f4e7f12a9de
Federico Falcón De Pizzo <ffalcondepizzo@gmail.com>
2023-09-19 23:15:54 -0300

    Rutas, controladores, cors, axios

--- models/server.js
+++ models/server.js
@@ -1,35 +1,46 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 class Server{
+
     constructor(){
         this.app = express();
+
         // Seteo de puerto por defecto
         this.port = process.env.PORT || 3000;
 
-        // this.middleware();
+        // 1° en orden
+        this.middleware();
+
+        // 2° en orden
         this.routers();
     }
 
     listen(){
         this.app.listen(this.port, () => {
-            console.log(`Server escuchando en el puerto: ${this.port}`)
+            console.log(`Servercito escuchando en el puerto: ${this.port}`)
         })
     }
-    // middleware(){
-    //     // CORS
-    //     // TODO: Agregar alguna regla en caso que sea necesario
-    //     this.app.use(cors());
-        
-    //     // Una vez configurada la carpeta pública, la ruta raiz
-    //     this.app.use(express.static('public'));
-    // }
+    
+    middleware(){
+        // CORS
+        // TODO: Agregar alguna regla en caso que sea necesario
+        this.app.use(cors());
+
+        // Una vez que se configura esta sección pública,
+        // NO es posible utilizar la ruta del inicio ('Home')
+        this.app.use(express.static('public'));
+
+    }
 
     routers(){
-        this.app.get('/', (req, res) => {
-            res.send('Hello World!')
-        })
+        // this.app.get('/', (req, res) => {
+        //     res.send('Home')
+        // })
+
+        this.app.use('/api/v1/demo', require('../routes/demo'));
     }
+
 }
 
 module.exports = Server;
